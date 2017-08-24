4f5441619292aed1ad901d0125b3b2c9b556a478
Himanshu Dixit <hudixt@gmail.com>
2017-08-24 12:36:59 +0530

    Initial Commit [Using Theleague library]

---
+++ src/Plugin/Network/BoxAuthInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\social_auth_box\Plugin\Network;
+
+use Drupal\social_api\Plugin\NetworkInterface;
+
+/**
+ * Defines the Box Auth interface.
+ */
+interface BoxAuthInterface extends NetworkInterface {}
